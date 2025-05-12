cfcb23f51b8aea654ba20cbf8760f61bb7e4df3f
Darya <darya.martyniuk@student.pk.edu.pl>
2025-05-12 03:41:53 +0200

    Maked fields private, add getters and setters

--- src/main/java/com/martyniuk/testTask/model/Order.java
+++ src/main/java/com/martyniuk/testTask/model/Order.java
@@ -4,10 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    public String id;
-    public double value;
-    public List<String> promotions = new ArrayList<>();
+    private String id;
+    private double value;
+    private List<String> promotions = new ArrayList<>();
 
     public Order() {}
 
+    public String getId() {
+        return id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public List<String> getPromotions() {
+        return promotions;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void setPromotions(List<String> promotions) {
+        this.promotions = promotions;
+    }
 }
--- src/main/java/com/martyniuk/testTask/model/PaymentMethod.java
+++ src/main/java/com/martyniuk/testTask/model/PaymentMethod.java
@@ -1,9 +1,33 @@
 package com.martyniuk.testTask.model;
 
 public class PaymentMethod {
-    public String id;
-    public int discount;
-    public double limit;
+    private String id;
+    private int discount;
+    private double limit;
 
     public PaymentMethod() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
 }
